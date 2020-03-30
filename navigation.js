bedf91802b96d02604d7baf4e907971818d3a384
DWWM <dwwm@cefimmac34.local>
2020-03-30 14:33:45 +0200

    Maj 1.0 - Ajout de la fonction noscroll

--- navigation.js
+++ navigation.js
@@ -3,6 +3,7 @@ const menu = document.querySelector('.navigation ul');
 
 toggleMenu.addEventListener('click', function() {
 	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
+	document.body.classList.toggle('noscroll');
 	toggleMenu.setAttribute('aria-expanded', !open);
 	menu.hidden = !menu.hidden;
 });
